336c1412ae92667385bec782860a7b1767c82d78
shikeik <2667097598@qq.com>
2025-01-20 02:53:23 +0800

    往返巡逻的敌人

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 115.0
 const JUMP_VELOCITY = -300.0
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
