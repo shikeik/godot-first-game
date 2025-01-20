ba521e6c8d0f9b3b66c78bb6e95f16068d44b2d3
shikeik <2667097598@qq.com>
2025-01-20 16:15:37 +0800

    添加音乐与拾取金币音效

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var anim_player = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free()
+	anim_player.play("pickup")
